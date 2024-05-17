10202fb61dc4a5f1b6c5257a2acd6a7ac5af05c3
venkatsurya <venkatsurya1608@gmail.com>
2024-05-17 15:36:42 +0530

    dynamic

---
+++ dynamic/sg.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "allow_ports" {
+  name        = "allow_ports"
+  description = "allowing 22,80,8080,3306 access"
+  
+  dynamic "ingress" {
+    for_each = var.inbound_rules
+    content {
+        from_port        = ingress.value["port"]  #each.value each.[key name]
+        to_port          = ingress.value["port"]
+        protocol         = ingress.value["protocol"]
+        cidr_blocks      = ingress.value["allowed_cidr"]
+    }
+
+}
+ 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"] 
+  }
+
+  tags = {
+    Name = "allow_ssh"
+    createdBy = "venkat"
+}
+
+}
