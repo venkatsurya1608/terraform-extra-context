2f5b64b0f2cfa8bef763d346809b69cb840e0b01
venkatsurya <venkatsurya1608@gmail.com>
2024-05-22 15:14:58 +0530

    vpc-cidr

---
+++ terraform-vpc-expense/data.tf
@@ -0,0 +1,8 @@
+data "aws_availability_zones" "available" {    ## terraform Data Source: aws_availability_zones
+    state = "available"
+}
+
+# data "aws_arm" "available" {
+#     state = "available"
+  
+# }
---
+++ terraform-vpc-expense/outputs.tf
@@ -0,0 +1,8 @@
+# output "azs" {                                             # if you want only zones names mention this .names
+#    value = data.aws_availability_zones.available.names    #all available zone names are here showing # mention names ani pakka evvali zones names vasthai
+   
+#  }
+
+# output "arm_vpc" {
+#     value = data.aws_arm.available
+# }
